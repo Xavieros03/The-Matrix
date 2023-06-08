79a498c82c183f0c39568d2982aeec2437c4cd3e
Xavier Ortega <xaviersendra@hotmail.com>
2023-06-08 19:27:16 +0200

    Player

--- game.js
+++ game.js
@@ -1,14 +1,45 @@
 const canvas = document.getElementById("gameCanvas")
 const ctx = canvas.getContext("2d")
 
+//player related variables
+
+const playerWidth = 20
+const playerHeight = 20
+let playerX = canvas.width / 2 - playerWidth / 2
+let playerY = canvas.height / 2 - playerHeight / 2
+
+//playing ground related variables
+
+const boxWidth = 1400
+const boxHeight = 700
+const boxX = (canvas.width - boxWidth) / 2
+const boxY = (canvas.height - boxHeight) / 2
+
 
 const startButton = document.getElementById("startButton")
 function startGame() {
     
   
-    ctx.fillStyle = "red";
-    ctx.fillRect(50, 50, 100, 100);
+  function drawPlayer() {
+    ctx.fillStyle = "blue"
+    ctx.fillRect(playerX, playerY, playerWidth, playerHeight)
   }
+ 
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
   
-  startButton.addEventListener("click", startGame);
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+  
+    ctx.strokeStyle = "black"
+    ctx.lineWidth = 4
+    ctx.strokeRect(boxX, boxY, boxWidth, boxHeight)
+  
+    drawPlayer()
+    
+  }
+  draw()
+}
+  
+  startButton.addEventListener("click", startGame)
 
